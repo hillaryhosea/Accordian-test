3f0604e7ec84e94c52c7172975f8bdf7a86a2774
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2024-07-08 23:28:33 +0300

    singular selection additions

--- src/Components/Accordian/Accordian.jsx
+++ src/Components/Accordian/Accordian.jsx
@@ -1,20 +1,32 @@
 import React, { useState } from 'react'
 import data from './data';
+import './Accordian.css'
 
 export const Accordian = () => {
 
   const [selected, setSelected] = useState(null);
 
+  function  handleSingleSelector (getCurrentId){
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+
+  }
+
   return (
     <div className='wrapper'>
+    <button> Enable multsgitelection</button>
       <div className='accordian'>
         {
           data && data.length > 0 ?
           data.map(dataItem => <div className='item'>
-            <div className='title'>
+            <div onClick={()=>handleSingleSelector(dataItem.id)} className='title'>
               <h3>{dataItem.question}</h3>
               <span>+</span>
             </div>
+            {
+              selected === dataItem.id ?
+               <div className='content'>{dataItem.answer}</div>
+               :null
+            }
           </div>)
           :<div>No data found !</div>
         }
